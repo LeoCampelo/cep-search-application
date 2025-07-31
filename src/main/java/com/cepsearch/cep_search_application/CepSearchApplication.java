dccd75b62f4e88e6f0619781e1d144a3cbe12a54
Leo Campelo <leocampelo16@gmail.com>
2025-07-31 15:14:18 -0300

    Initial Commit

---
+++ src/main/java/com/cepsearch/cep_search_application/CepSearchApplication.java
@@ -0,0 +1,13 @@
+package com.cepsearch.cep_search_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepSearchApplication.class, args);
+	}
+
+}
